684453191138e9bfb557cbe28c18609a1cad31b9
Dmitry Malinovsky <maldavak@icloud.com>
2022-02-25 01:03:22 +0500

    21.02.2022

--- task.php
+++ task.php
@@ -1,4 +1,5 @@
 <?php
+//задача с отправкой емаил
 if (empty($_POST)){
     $_POST = ['to'=>'1','subject'=>'','message'=>''];
 }else {
@@ -6,6 +7,22 @@ if (empty($_POST)){
     $subject = wordwrap($_POST['subject'], 70, "\r\n");
     $message = $_POST['message'];
     mail($to, $subject, $message);
+}
+//задача №4 от 21.02.2022
+$text1 = file_get_contents('text1.txt');
+$text2 = file_get_contents('text2.txt');
+preg_match_all("/.*?[.?!](?:\s|$)/s", $text1,$items1);
+preg_match_all("/.*?[.?!](?:\s|$)/s", $text2,$items2);
+foreach ($items1[0] as $key1){
+    foreach ($items2[0] as $key2){
+        //повторяющиеся предложения
+        if ($key1 == $key2){
+            fwrite(fopen('textresult.txt', 'r+'), "$key1");
+        }elseif ($key1 != $key2){       //неповторяющиеся предложения
+            fwrite(fopen('textresult2.txt', 'r+'), "$key1");
+            fwrite(fopen('textresult2.txt', 'r+'), "$key2");
+        }
+    }
 }
 ?>
 <!doctype html>
